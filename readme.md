f560d2c707aa0fa206ab58289624e128e34daf21
Thiago Pereira <thiagopereiramail@gmail.com>
2025-11-20 15:29:22 -0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Arquivos de configuração
+
+## Como executar um script em outro diretório
+
+```bash
+make -f sway/debian-minimal.sh test
+```
---
+++ sway/debian-minimal.sh
@@ -0,0 +1,44 @@
+install_deps:
+	@echo "Instalar as dependências"
+	sudo apt update
+
+	# instalar aplicativos
+	sudo apt install -y nwg-look wdisplays foot thunar\
+		pipewire alsa-utils pwvucontrol bluetooth blueman wofi\
+		bash-completion neovim kclock htop firefox-esr
+	
+	# instalar o sway
+	sudo apt install -y sway sway-backgrounds swaybg xwayland greetd wlgreet\
+		swappy mako-notifier
+
+	# remover recursos não utilizados
+	sudo apt autoremove -y
+
+
+sys_config:
+	@echo "Configurações do sistema"
+
+	# criar diretórios de configuração
+	mkdir -p ~/.config/sway
+	mkdir -p ~/.config/waybar
+	mkdir -p ~/.config/wofi
+
+	# criar diretórios de assets
+	mkdir -p ~/.local/share/fonts
+	mkdir -p ~/.local/share/applications
+	mkdir -p ~/.local/wallpapers
+	mkdir -p ~/.local/bin
+
+	# copiar configurações
+	cp -f sway/config.conf ~/.config/sway/config
+	cp -f sway/waybar/config.conf ~/.config/waybar/config
+	cp -f sway/wofi/config.conf ~/.config/wofi/config
+
+	# copiar assets
+	cp -f fonts/*.ttf ~/.local/share/fonts
+	cp -f shortcuts/*.desktop ~/.local/share/applications
+	cp -f wallpapers/* ~/.local/wallpapers
+
+
+test:
+	@echo "configure debian minimal"
