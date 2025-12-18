e46e33948e179b03489bc8e3845a044521bdc2ca
Thiago Pereira <thiagopereiramail@gmail.com>
2025-12-18 07:44:35 -0300

    refactor: remove rhythmbox

--- sway/debian-minimal.sh
+++ sway/debian-minimal.sh
@@ -11,7 +11,7 @@ install_deps:
 		pipewire pipewire-pulse pipewire-alsa\
 		pipewire-audio-client-libraries wireplumber wireplumber-bin pwvucontrol\
 		bluetooth libspa-0.2-bluetooth blueman wofi bash-completion neovim\
-		rhythmbox htop gdm3 papirus-icon-theme
+		htop gdm3 papirus-icon-theme
 	
 	# instalar o sway
 	sudo apt install -y --no-install-recommends --no-install-suggests\
