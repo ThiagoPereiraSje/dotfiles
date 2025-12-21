acb4b956af765886b9f41466d48ec6cdf6e344ff
Thiago Pereira <thiagopereiramail@gmail.com>
2025-12-21 08:41:33 -0300

    refactor: mic volume

--- sway/waybar/mic_vol.sh
+++ sway/waybar/mic_vol.sh
@@ -4,7 +4,7 @@ OUT=$(LC_ALL=C wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
 VOL=$(echo "$OUT" | awk '{print $2}')
 
 if echo "$OUT" | grep -q MUTED; then
-  echo " ${VOL}%"
+  echo "<span foreground='#4c7899'></span> ${VOL}%"
 else
-  echo " ${VOL}%"
+  echo ""
 fi
