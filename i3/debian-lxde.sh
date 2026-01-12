91601825e8855a5f08cf643c3e6fd4d883fb9f15
Thiago Pereira <thiagopereiramail@gmail.com>
2026-01-12 14:36:12 -0300

    feat: add i3status config file

--- i3/debian-lxde.sh
+++ i3/debian-lxde.sh
@@ -1,20 +1,36 @@
 .SILENT:
 
-install: install_deps i3_config
+install: install_deps install_brave i3_config
 
 install_deps:
 	sudo apt update
 
 	# instalar dependências
-	sudo apt install -y i3 rofi picom
+	sudo apt install -y\
+		i3 rofi picom papirus-icon-theme bash-completion htop
+	
+	# remover dependências quebradas
+	sudo apt autoremove -y
 
 	# finalizar
 	echo "Instalação das dependências concluída com sucesso!"
 
 
+install_brave:
+	sudo apt install -y curl
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+	sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+	sudo apt update
+	sudo apt install -y brave-browser
+
+	# finalizar
+	echo "Instalação do brave finalizada com sucesso!"
+
+
 i3_config:
 	# diretórios de configuração
 	mkdir -p ~/.config/i3
+	mkdir -p ~/.config/i3status
 	mkdir -p ~/.config/rofi
 
 	# assets
@@ -26,6 +42,7 @@ i3_config:
 
 	# copiar as configurações
 	cp -f i3/config-lxde.conf ~/.config/i3/config
+	cp -f i3/i3status.conf ~/.config/i3status/config
 	cp -f i3/rofi/config.conf ~/.config/rofi/config.rasi
 
 	# copiar os assets
