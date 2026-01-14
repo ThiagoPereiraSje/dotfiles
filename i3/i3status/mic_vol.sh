27bb8b19eb01050ab66b78ecbe92102a6b7db68d
Thiago Pereira <thiagopereiramail@gmail.com>
2026-01-14 07:43:28 -0300

    wip: work on microfone volume

---
+++ i3/i3status/mic_vol.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+OUT=$(LC_ALL=C wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
+VOL=$(echo "$OUT" | awk '{print $2}')
+
+if echo "$OUT" | grep -q MUTED; then
+  echo "MIC: ${VOL}%"
+else
+  echo "MIC: MUTE"
+fi
