49968bcefd301b67ba2a6756c9cfae716328ef34
Thiago Pereira <thiagopereiramail@gmail.com>
2026-01-14 11:36:42 -0300

    fix: i3mic script

--- i3/i3blocks/i3mic.sh
+++ i3/i3blocks/i3mic.sh
@@ -4,7 +4,7 @@ OUT=$(LC_ALL=C wpctl get-volume @DEFAULT_AUDIO_SOURCE@)
 VOL=$(echo "$OUT" | awk '{print $2}')
 
 if echo "$OUT" | grep -q MUTED; then
-  echo "MIC: ${VOL}%"
-else
   echo "MIC: MUTE"
+else
+  echo "MIC: ${VOL}%"
 fi
