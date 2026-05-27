f4604087bc6c7126bcd1099ea27cb6f4d7fe327e
Thiago Pereira <thiagopereiramail@gmail.com>
2026-05-27 15:43:11 -0300

    chore: add powermenu

---
+++ i3/i3blocks/powermenu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+chosen=$(printf " Desligar\n Reiniciar\n Logout\n" | \
+rofi -dmenu -i -p "Power Menu")
+
+case "$chosen" in
+    " Desligar")
+        systemctl poweroff
+        ;;
+    " Reiniciar")
+        systemctl reboot
+        ;;
+    " Logout")
+        i3-msg exit
+        ;;
+esac
