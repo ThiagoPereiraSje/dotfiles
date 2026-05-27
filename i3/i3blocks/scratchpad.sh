d11f1de23d6abff1b3fd37ba8d7bcc94680b2f34
Thiago Pereira <thiagopereiramail@gmail.com>
2026-05-27 11:30:59 -0300

    refactor: scratchpad script

--- i3/i3blocks/scratchpad.sh
+++ i3/i3blocks/scratchpad.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-i3-msg -t get_tree | jq -r '
-recurse(.nodes[]?, .floating_nodes[]?)
-| select(.scratchpad_state != "none")
-| (.name // .window_properties.class // empty)
-' | paste -sd " | " -
+count=$(i3-msg -t get_tree | jq '
+[
+  recurse(.nodes[]?, .floating_nodes[]?)
+  | select(.scratchpad_state != "none")
+] | length
+')
+
+echo " $count"
