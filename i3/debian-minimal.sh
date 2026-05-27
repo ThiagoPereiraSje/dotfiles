4e91d142e5bc31eb839033e87c572a2215adfa8b
Thiago Pereira <thiagopereiramail@gmail.com>
2026-05-27 16:30:52 -0300

    chore: add bluetooth deps

--- i3/debian-minimal.sh
+++ i3/debian-minimal.sh
@@ -10,7 +10,7 @@ install_deps:
 		xorg lightdm suckless-tools pipewire wireplumber\
 		lxappearance lxrandr lxtask lxterminal pcmanfm\
 		network-manager network-manager-gnome\
-		bluetooth blueman alsa-utils\
+		bluetooth libspa-0.2-bluetooth blueman alsa-utils\
 		i3 i3blocks rofi picom feh bash-completion htop jq maim slop neovim
 
 	# ativar o gerenciador de rede
