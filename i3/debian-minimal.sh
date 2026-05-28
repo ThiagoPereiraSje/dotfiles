b4b632a8f0c97b8eaf2405578ad57708e19d1ef7
Thiago Pereira <thiagopereiramail@gmail.com>
2026-05-28 09:16:58 -0300

    chore: add brave

--- i3/debian-minimal.sh
+++ i3/debian-minimal.sh
@@ -65,3 +65,15 @@ i3_config:
 
 	# finalizar
 	echo "Configuração finalizada com sucesso!"
+
+
+install_brave:
+	sudo apt install -y curl
+	
+	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+	sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+
+	sudo apt update
+	sudo apt install -y brave-browser
+
+	echo "Instalação do brave finalizada com sucesso!"
